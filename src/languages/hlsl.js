0785270d3ba2dc3d5bc9c20e5c7aa76de47c3a7c
Jon Leech <oddhack@sonic.net>
2023-05-07 22:15:20 -0700

    Add eslint configuration from the Antora Default UI project (https://gitlab.com/antora/antora-ui-default) and make hlsl.js pass it.

--- src/languages/hlsl.js
+++ src/languages/hlsl.js
@@ -5,53 +5,52 @@ Author: Stef Levesque
 Website: https://docs.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl
 Category: graphics
 */
-
-const HLSL_NUMBER_RE = '(-?)(\\b0[xX][a-fA-F0-9]+|(\\b\\d+(\\.\\d*)?([hHfFlL]?)|\\.\\d+)([eE][-+]?\\d+)?([hHfFlL]?))'; // 0x..., 0..., decimal, float, half, double
+// 0x..., 0..., decimal, float, half, double
+const HLSL_NUMBER_RE = '(-?)(\\b0[xX][a-fA-F0-9]+|(\\b\\d+(\\.\\d*)?([hHfFlL]?)|\\.\\d+)([eE][-+]?\\d+)?([hHfFlL]?))'
 
 const HLSL_NUMBER_MODE = {
   className: 'number',
   begin: HLSL_NUMBER_RE,
-  relevance: 0
-};
+  relevance: 0,
+}
 
-export default function(hljs) {
-
-  let matrixBases = 
+export default function (hljs) {
+  const matrixBases =
     'bool double float half int uint ' +
-    'min16float min10float min16int min12int min16uint';
-    
-  let matrixSuffixes = [
+    'min16float min10float min16int min12int min16uint'
+
+  const matrixSuffixes = [
     '',
     '1', '2', '3', '4',
     '1x1', '1x2', '1x3', '1x4',
     '2x1', '2x2', '2x3', '2x4',
     '3x1', '3x2', '3x3', '3x4',
-    '4x1', '4x2', '4x3', '4x4'];
+    '4x1', '4x2', '4x3', '4x4']
 
-  let matrixTypes = [];
+  const matrixTypes = []
 
-  for (let base of matrixBases.split(' ')) {
-    for (let suffix of matrixSuffixes) {
-      matrixTypes.push(base + suffix);
+  for (const base of matrixBases.split(' ')) {
+    for (const suffix of matrixSuffixes) {
+      matrixTypes.push(base + suffix)
     }
   }
 
-  let semanticsSV = 
+  const semanticsSV =
     'SV_Coverage SV_Depth SV_DispatchThreadID SV_DomainLocation ' +
     'SV_GroupID SV_GroupIndex SV_GroupThreadID SV_GSInstanceID SV_InnerCoverage SV_InsideTessFactor ' +
     'SV_InstanceID SV_IsFrontFace SV_OutputControlPointID SV_Position SV_PrimitiveID ' +
     'SV_RenderTargetArrayIndex SV_SampleIndex SV_StencilRef SV_TessFactor SV_VertexID ' +
-    'SV_ViewportArrayIndex, SV_ShadingRate';
-  
-  let semanticsNum = 
+    'SV_ViewportArrayIndex, SV_ShadingRate'
+
+  const semanticsNum =
     'BINORMAL BLENDINDICES BLENDWEIGHT COLOR NORMAL POSITION PSIZE TANGENT TEXCOORD TESSFACTOR DEPTH ' +
     'SV_ClipDistance SV_CullDistance SV_DepthGreaterEqual SV_DepthLessEqual SV_Target ' +
-    'SV_CLIPDISTANCE SV_CULLDISTANCE SV_DEPTHGREATEREQUAL SV_DEPTHLESSEQUAL SV_TARGET';
+    'SV_CLIPDISTANCE SV_CULLDISTANCE SV_DEPTHGREATEREQUAL SV_DEPTHLESSEQUAL SV_TARGET'
 
-  let semanticsTypes = semanticsNum.split(' ');
+  const semanticsTypes = semanticsNum.split(' ')
 
-  for (let s of semanticsNum.split(' ')) {
-    for (let n of Array(16).keys()) {
+  for (const s of semanticsNum.split(' ')) {
+    for (const n of Array(16).keys()) {
       semanticsTypes.push(s + n.toString())
     }
   }
@@ -106,7 +105,7 @@ export default function(hljs) {
         'tex2Dlod tex2Dproj tex3D tex3Dbias tex3Dgrad tex3Dlod tex3Dproj texCUBE texCUBEbias texCUBEgrad texCUBElod ' +
         'texCUBEproj transpose trunc',
 
-      literal: 'true false'
+      literal: 'true false',
     },
     illegal: '"',
     contains: [
@@ -115,8 +114,9 @@ export default function(hljs) {
       HLSL_NUMBER_MODE,
       {
         className: 'meta',
-        begin: '#', end: '$'
-      }
-    ]
-  };
+        begin: '#',
+        end: '$',
+      },
+    ],
+  }
 }
